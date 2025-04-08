63e9109865e6406c8bb05da86ad59c595a9a6536
theisaachome <65864160+theisaachome@users.noreply.github.com>
2025-04-09 00:28:54 +0800

    error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -17,22 +17,28 @@ const FormSchema = z.object({
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
  
-
 export async function createInvoice(formData: FormData) {
-   const { customerId, amount, status } = CreateInvoice.parse({
-     customerId: formData.get("customerId"),
-     amount: formData.get("amount"),
-     status: formData.get("status"),
-   });
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split("T")[0];
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
 
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
-    revalidatePath("/dashboard/invoices");
-    redirect("/dashboard/invoices");
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    // We'll log the error to the console for now
+    console.error(error);
+  }
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
 
 // Use Zod to update the expected types
@@ -49,17 +55,22 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
- 
+  try {
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+  } catch (error) {
+    // We'll log the error to the console for now
+    console.error(error);
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
+  throw new Error("Failed to Delete Invoice");
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
 }
